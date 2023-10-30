89ea7e126748996334c37e99aff7816ad7f615a0
carlapariasm <145057328+carlapariasm@users.noreply.github.com>
2023-10-30 13:46:38 +0100

    Create tensor_operations.py

---
+++ module_structure/tensor_operations.py
@@ -0,0 +1,18 @@
+import torch
+
+
+__all__ = ['TensorCalculator']
+
+class TensorCalculator():
+
+    def __init__(self):
+
+        return None
+
+    def tensor_ones(dim_x,
+                    dim_y,
+                    dim_z):
+
+        ones = torch.ones([dim_x,dim_y,dim_z])
+
+        return ones
